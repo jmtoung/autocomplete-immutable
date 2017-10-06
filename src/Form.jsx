54ad5d7b950ec71d8a900f9efa4050690341c5a0
jmtoung@gmail.com <jmtoung@gmail.com>
2017-10-05 17:05:23 -0700

    Add functions for solutions; turned off by default

--- src/Form.jsx
+++ src/Form.jsx
@@ -10,10 +10,54 @@ import {
 import { getCountries } from './creator';
 
 class Form extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      countries: [],
+      countriesList: [],
+      countryNameToCode: {},
+      countryCodeToName: {},
+    };
+  }
+
+  initializeCountryValues = (countries) => {
+    const countriesList = [];
+    const countryNameToCode = {};
+    const countryCodeToName = {};
+    for (const country of countries) {
+      const text = country.get("text");
+      const value = country.get("value");
+      countriesList.push(text);
+      countryNameToCode[text] = value;
+      countryCodeToName[value] = text;
+    }
+    this.setState(Object.assign({}, this.state, {
+      countries: countries.toJS(),
+      countriesList: countriesList,
+      countryNameToCode: countryNameToCode,
+      countryCodeToName: countryCodeToName,
+    }));
+    console.log(countriesList);
+  }
 
   componentWillMount() {
     this.props.getCountries();
   };
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.countries.size === 0 && nextProps.countries.size > 0) {
+      this.initializeCountryValues(nextProps.countries);
+    }
+  }
+
+  formatCountry = (value, name) => {
+    return this.state.countryCodeToName[value] || value;
+  }
+
+  parseCountry = (value, name) => {
+    return this.state.countryNameToCode[value] || value;
+  }
 
   render() {
     return (
@@ -36,8 +80,10 @@ class Form extends Component {
             floatingLabelFixed
             openOnFocus
             filter={MUIAutoComplete.fuzzyFilter}
+            //parse={this.parseCountry}
+            //format={this.formatCountry}
             dataSourceConfig={{text: 'text', value: 'value'}}
-            dataSource={this.props.countries.toJS()}
+            dataSource={this.state.countries}
           />
         </div>
       </div>
--- src/creator.js
+++ src/creator.js
@@ -11,6 +11,7 @@ function retrieveCountries() {
       {value: 'EG', text: 'Egypt'},
       {value: 'FI', text: 'Finland'},
       {value: 'FR', text: 'France'},
+      {value: 'US', text: 'United States'},
   ];
 
   return new Promise((resolve, reject) => {
@@ -31,5 +32,81 @@ export function getCountries() {
   return async (dispatch) => {
     const response = await retrieveCountries();
     dispatch(getCountriesSuccess(response));
+  };
+}
+
+function retrieveCities(country) {
+  const cities = {
+    'AF': [
+      {value: 'KA', text: 'Kabul'},
+      {value: 'HE', text: 'Herat'},
+      {value: 'GH', text: 'Ghazni'},
+    ],
+    'AR': [
+      {value: 'BA', text: 'Buenos Aires'},
+      {value: 'MP', text: 'Mar del Plata'},
+    ],
+    'BE': [
+      {value: 'BU', text: 'Bruges'},
+      {value: 'GH', text: 'Ghent'},
+    ],
+    'BR': [
+      {value: 'RI', text: 'Rio de Janeiro'},
+      {value: 'SP', text: 'Sao Paulo'},
+    ],
+    'CA': [
+      {value: 'TO', text: 'Toronto'},
+      {value: 'OT', text: 'Ottawa'},
+      {value: 'MT', text: 'Montreal'},
+      {value: 'VC', text: 'Vancouver'},
+    ],
+    'CN': [
+      {value: 'BJ', text: 'Beijing'},
+      {value: 'SH', text: 'Shanghai'},
+      {value: 'SZ', text: 'Shenzhen'},
+    ],
+    'DK': [
+      {value: 'CP', text: 'Copenhagen'},
+      {value: 'AA', text: 'Aarhus'},
+    ],
+    'EG': [
+      {value: 'CO', text: 'Cairo'},
+      {value: 'AX', text: 'Alexandria'},
+      {value: 'GZ', text: 'Giza'},
+    ],
+    'FI': [
+      {value: 'HS', text: 'Helsinki'},
+      {value: 'TP', text: 'Tampere'},
+    ],
+    'FR': [
+      {value: 'PS', text: 'Paris'},
+      {value: 'MS', text: 'Marseille'},
+      {value: 'BX', text: 'Bordeaux'},
+    ],
+    'US': [
+      {value: 'NY', text: 'New York'},
+      {value: 'SF', text: 'San Francisco'},
+      {value: 'MP', text: 'Menlo Park'},
+    ],
+  };
+
+  return new Promise((resolve, reject) => {
+    setTimeout(function() {
+      resolve(cities[country]);
+    }, 2000)
+  });
+}
+
+export function getCitiesSuccess(cities) {
+  return {
+    type: 'GET_CITIES_SUCCESS',
+    cities
+  }
+}
+
+export function getCities(country) {
+  return async (dispatch) => {
+    const response = await retrieveCities(country);
+    dispatch(getCitiesSuccess(response));
   };
 }
